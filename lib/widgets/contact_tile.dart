0e0ad0a6e5f8432d7409ae6d340d9e97e5cb7f7a
Abdelrahman <AbdelRahman.AlHajHussein@equiti.com>
2023-11-18 17:48:28 +0300

    finish the tile

--- lib/widgets/contact_tile.dart
+++ lib/widgets/contact_tile.dart
@@ -6,23 +6,51 @@ class ContactTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 70,
+      height: 80,
       child: Padding(
         padding: const EdgeInsets.only(left: 16, right: 16, top: 8),
-        child: Row(
+        child: Column(
           children: [
-            CircleAvatar(
-              radius: 25,
-              backgroundImage: NetworkImage(
-                  'https://media.istockphoto.com/id/1300512215/photo/headshot-portrait-of-smiling-ethnic-businessman-in-office.jpg?s=612x612&w=0&k=20&c=QjebAlXBgee05B3rcLDAtOaMtmdLjtZ5Yg9IJoiy-VY='),
-              backgroundColor: Colors.transparent,
+            Row(
+              children: [
+                const CircleAvatar(
+                  radius: 25,
+                  backgroundImage: NetworkImage(
+                      'https://media.istockphoto.com/id/1300512215/photo/headshot-portrait-of-smiling-ethnic-businessman-in-office.jpg?s=612x612&w=0&k=20&c=QjebAlXBgee05B3rcLDAtOaMtmdLjtZ5Yg9IJoiy-VY='),
+                  backgroundColor: Colors.transparent,
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            const Text(
+                              "Abed alrahman al hajj",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            Expanded(child: Container()),
+                            const Text(
+                              "10:00 PM",
+                              style: TextStyle(color: Colors.blue),
+                            ),
+                          ],
+                        ),
+                        const Text(
+                          "chat descriptions",
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "Abed alrahman al hajj",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
+            const Padding(
+              padding: EdgeInsets.only(left: 60),
+              child: Divider(),
             )
           ],
         ),
